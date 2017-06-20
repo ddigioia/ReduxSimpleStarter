339a79234a48fbf205902538ded4aa0b276a88d9
Derek DiGioia <derekdigioia@Dereks-MacBook-Pro.local>
2017-06-20 07:40:01 -0700

    more setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyCviVkYbTGa7WVJsAcQp0Ao7ogbo7fkFwM'
+
+const App = () => {
+  return <div>Hi!</div>
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
